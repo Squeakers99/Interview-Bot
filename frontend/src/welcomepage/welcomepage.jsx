3762a1d0abdd0685eb06ccbfb25a58c483aa3ebe
shravyashah <shravya.shah066@gmail.com>
2026-02-20 14:58:21 -0700

    Add landing page JSX

---
+++ frontend/src/welcomepage/welcomepage.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react";
+import {useNavigate} from "react-router-dom";
+
+function LandingPage() { 
+    const navigate = useNavigate();
+
+    const handleStart = () => {
+        navigate("/App")
+
+
+    };
+
+
+
+
+
+}
