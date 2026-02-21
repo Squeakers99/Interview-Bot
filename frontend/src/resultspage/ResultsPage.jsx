9d0d876e9c88c0c76108bd2cb7003e1e9cee5410
shravyashah <shravya.shah066@gmail.com>
2026-02-20 21:52:20 -0700

    Editting ResultsPage.jsx

--- frontend/src/resultspage/ResultsPage.jsx
+++ frontend/src/resultspage/ResultsPage.jsx
@@ -16,5 +16,35 @@ import "./ResultsPage.css";
 export default function ResultsPage() {
     const location = useLocation();
     const navigate = useNavigate();
-
 }
+
+const [eyeHistory, setEyeHistory] = useState([]);
+const [postureHistory, setPostureHistory] = useState([]);
+const [loading, setLoading] = useState(state);
+const [error, setError] = useState(""); 
+
+useEffect(() => {
+    async function load () {
+        try {
+            const [eyeRes,postureRes] = await Promise.all([
+                fetch("http://127.0.0.1:8000/results/eye_timeline"),
+                fetch("htpps://127.0.0.1:8000/results/posture_timeline"),
+           ]);
+           const eyeJson = await eyeRes.json();
+           const postureJson = await postureRes();
+
+           setEyeHistory(eyeJson.eye_timeline || []);
+           setPostureHistory(postureJson.posture_timeline || []);
+
+
+        } catch(e) {
+          setError("Failed to load results");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    load();
+
+}, []);
+
