4d6d3bba665252c38819962956ee3988be4a32fc
Oghenekaro Egbevurie <kegbevurie@gmail.com>
2026-02-20 18:10:35 -0700

    Resultspage

---
+++ frontend/src/resultspage/resultspage.jsx
@@ -0,0 +1,30 @@
+import "./resultspage.css";
+import { useState } from "react";
+import App from "../App";
+
+export default function resultspage() {
+    const [started, setStarted] = useState(false);
+
+    if (started) {
+        return <App />;
+    }
+
+    return (
+        <div className="results-container">
+            <h1 className="results-title">
+                Results of Your Practice Interview
+            </h1>
+            <p className="results-subtitle">
+                Interview prompt:
+                <br />
+                Your response:
+                <br />
+                AI response:
+            </p>
+
+            <button className="results-button" onClick={() => setStarted(true)}>
+                Try-Again
+            </button>
+        </div>
+    );
+}
